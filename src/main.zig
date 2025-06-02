001eef23a9498a42c835c08235672ee48f54bffc
ajafri2001 <abdullahjafri2001@gmail.com>
2025-06-03 03:21:24 +0530

    sleepy will try tom

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const print = std.debug.print;
 const posix = std.posix;
 const linux = std.os.linux;
 
@@ -14,7 +15,7 @@ pub fn main() !void {
     const addr = std.net.Address.initIp4([4]u8{ 127, 0, 0, 1 }, 8080);
     const listener = try std.posix.socket(std.posix.AF.INET, 0x801, std.posix.IPPROTO.TCP);
 
-    std.debug.print("server on port 8080\n", .{});
+    print("server on port 8080\n", .{});
 
     try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
     try posix.bind(listener, &addr.any, addr.getOsSockLen());
@@ -30,7 +31,7 @@ pub fn main() !void {
 
     var ready_list: [256]linux.epoll_event = undefined;
 
-    std.debug.print("listening...\n", .{});
+    print("listening...\n", .{});
 
     while (true) {
         const ready_count = posix.epoll_wait(fd, &ready_list, -1);
@@ -42,9 +43,11 @@ pub fn main() !void {
                 var event = linux.epoll_event{ .events = linux.EPOLL.IN, .data = .{ .fd = client_socket } };
                 try posix.epoll_ctl(fd, linux.EPOLL.CTL_ADD, client_socket, &event);
             } else {
-                var buf: [1024]u8 = undefined;
-                const read = posix.read(ready_socket, &buf) catch 0;
-                if (read == 0 or (ready.events & linux.EPOLL.RDHUP) != 0) {
+                var buf: [1024]u8 = undefined; // stack > heap
+                const request_size: usize = posix.read(ready_socket, buf[0..]) catch 0;
+                print("{s}", .{buf[0..request_size]});
+                if (request_size == 0 or (ready.events & linux.EPOLL.RDHUP) != 0) {
+                    // parseHttpReq(buf[0..request_size]);
                     try posix.epoll_ctl(fd, linux.EPOLL.CTL_DEL, ready_socket, null);
                     posix.close(ready_socket);
                 } else {
@@ -55,4 +58,21 @@ pub fn main() !void {
             }
         }
     }
+}
+
+const HttpRequest = struct {
+    method: []const u8,
+    path: []const u8,
+    version: []const u8,
+    // umm the header_key will have ":" at the end :sweat
+    headers: []const struct { header_key: []const u8, header_value: []const u8 },
+    body: []const u8,
+};
+
+pub fn parseHttpReq(to_parse: []u8) !HttpRequest {
+    const lines = std.mem.splitSequence(u8, to_parse, "\r\n");
+
+    const method = std.mem.splitScalar([]u8, lines[0], " ").first;
+    const path = method.next();
+    const version = method.next();
 }
